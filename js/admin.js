13f84134c1700d34fa31d677c3267f58d74ba3f7
ichiky0 <shivkumarschoolid@gmail.com>
2026-07-09 15:41:47 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,73 @@
-const btn = document.getElementById("saveBtn");
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-btn.addEventListener("click", () => {
+const CLOUD_NAME = "e1pknxmd";
+const UPLOAD_PRESET = "chindi_upload";
 
-    alert("Next Step: Upload to Cloudinary");
+document.getElementById("saveBtn").addEventListener("click", saveProduct);
 
-});
+async function saveProduct() {
+
+    const name = document.getElementById("name").value.trim();
+    const price = Number(document.getElementById("price").value);
+    const category = document.getElementById("category").value;
+    const description = document.getElementById("description").value.trim();
+    const stock = Number(document.getElementById("stock").value);
+    const imageFile = document.getElementById("image").files[0];
+
+    if (!name || !price || !imageFile) {
+        alert("Please fill all required fields.");
+        return;
+    }
+
+    try {
+
+        document.getElementById("status").innerHTML = "Uploading image...";
+
+        const formData = new FormData();
+        formData.append("file", imageFile);
+        formData.append("upload_preset", UPLOAD_PRESET);
+
+        const upload = await fetch(
+            `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const imageData = await upload.json();
+
+        document.getElementById("status").innerHTML = "Saving product...";
+
+        await addDoc(collection(db, "products"), {
+
+            name,
+            price,
+            category,
+            description,
+            stock,
+
+            image: imageData.secure_url,
+
+            createdAt: new Date()
+
+        });
+
+        document.getElementById("status").innerHTML =
+            "✅ Product Added Successfully";
+
+    }
+    catch(err){
+
+        console.error(err);
+
+        document.getElementById("status").innerHTML =
+            "❌ Error adding product.";
+
+    }
+
+}
