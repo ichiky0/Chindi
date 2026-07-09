78992245aac6a82c2de7ba4a5667388998ef3aef
ichiky0 <shivkumarschoolid@gmail.com>
2026-07-09 13:44:48 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -2,21 +2,6 @@ const btn = document.getElementById("saveBtn");
 
 btn.addEventListener("click", () => {
 
-    const product = {
-        name: document.getElementById("name").value,
-        price: document.getElementById("price").value,
-        category: document.getElementById("category").value,
-        description: document.getElementById("description").value,
-        stock: document.getElementById("stock").value
-    };
-
-    let products = JSON.parse(localStorage.getItem("products")) || [];
-
-    products.push(product);
-
-    localStorage.setItem("products", JSON.stringify(products));
-
-    document.getElementById("status").innerHTML =
-    "✅ Product Saved Successfully";
+    alert("Next Step: Upload to Cloudinary");
 
 });
