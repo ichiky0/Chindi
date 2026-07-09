772fcb7a10ea746dc2a008f74a32465e365952a2
ichiky0 <shivkumarschoolid@gmail.com>
2026-07-09 13:42:00 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,22 @@
 const btn = document.getElementById("saveBtn");
 
 btn.addEventListener("click", () => {
-    
-    alert("Next Step: Upload to Cloudinary");
-    
+
+    const product = {
+        name: document.getElementById("name").value,
+        price: document.getElementById("price").value,
+        category: document.getElementById("category").value,
+        description: document.getElementById("description").value,
+        stock: document.getElementById("stock").value
+    };
+
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+
+    products.push(product);
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    document.getElementById("status").innerHTML =
+    "✅ Product Saved Successfully";
+
 });
